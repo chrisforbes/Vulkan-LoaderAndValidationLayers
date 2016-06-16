88e8996af22b3e5770224ddd5ebfe8609803af94
Chris Forbes <chrisforbes@google.com>
2016-06-16 17:16:00 +1200

    layers: Get rid of fence->swapchain link
    Nothing used this after rationalizing the state tracking.
    
    Signed-off-by: Chris Forbes <chrisforbes@google.com>

--- layers/core_validation.cpp
+++ layers/core_validation.cpp
@@ -9866,7 +9866,6 @@ VKAPI_ATTR VkResult VKAPI_CALL AcquireNextImageKHR(VkDevice device, VkSwapchainK
     if (result == VK_SUCCESS || result == VK_SUBOPTIMAL_KHR) {
         if (pFence) {
             pFence->state = FENCE_INFLIGHT;
-            pFence->swapchain = swapchain;
         }
 
         // A successful call to AcquireNextImageKHR counts as a signal operation on semaphore
